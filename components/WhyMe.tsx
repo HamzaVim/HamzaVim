9ee26806964df0b814b8c722413fcdc1e9a18156
HamzaSx <Hamzasxh@gmail.com>
2025-05-03 20:45:58 +0300

    feat(WhyMe section: Hover animation):

--- components/WhyMe.tsx
+++ components/WhyMe.tsx
@@ -2,7 +2,7 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const ListItem = ({
   title, // Original title
@@ -23,19 +23,89 @@ const ListItem = ({
   // Ref: highlighted text
   const highlightedTextRef = useRef<HTMLHeadingElement>(null);
 
+  // Ref for the svg to hide the text
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  // State for the hover
+  const [hovered, setHovered] = useState(false);
+
   // NOTE: Functions & Animations: ---------------------------------------------------
-  useGSAP(() => {
-    if (!liRef.current || !highlightedTextRef.current) return;
-    gsap.to(highlightedTextRef.current, {
-      scrollTrigger: {
-        trigger: highlightedTextRef.current,
-        start: "top 94%",
-        end: "top 50%",
-        scrub: true,
-      },
-      width: "100%",
-    });
-  });
+
+  // Animation when scroll
+  useGSAP(
+    () => {
+      if (!liRef.current || !highlightedTextRef.current) return;
+      gsap.to(highlightedTextRef.current, {
+        scrollTrigger: {
+          trigger: highlightedTextRef.current,
+          start: "top 94%",
+          end: "top 50%",
+          scrub: true,
+        },
+        width: "100%",
+      });
+    },
+    { scope: liRef },
+  );
+
+  // Adding the svg viewBox to the svgs
+  useEffect(() => {
+    if (!liRef.current || !svgRef.current) return;
+
+    // Extracting the width and height of the link
+    const width = liRef.current.offsetWidth;
+    const height = liRef.current.offsetHeight;
+
+    // Setting the viewBox
+    svgRef.current.setAttribute("viewBox", `0 0 ${width} ${height}`);
+
+    // Handling when the browser is resized
+    const handleResize = () => {
+      if (!liRef.current || !svgRef.current) return;
+
+      // Extracting the width and height of the link
+      const afterResizewidth = liRef.current.offsetWidth;
+      const afterResizeheight = liRef.current.offsetHeight;
+
+      svgRef.current.setAttribute(
+        "viewBox",
+        `0 0 ${afterResizewidth} ${afterResizeheight}`,
+      );
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Animation when hovered
+  useGSAP(
+    () => {
+      if (!svgRef.current) return;
+
+      if (hovered) {
+        // When hovered we want to show the other text
+        gsap.to("rect", {
+          overwrite: "auto",
+          ease: "power2.out",
+          scaleY: 1,
+          duration: 0.4,
+        });
+      } else {
+        // When not hovered we want to hide the other text
+        gsap.to("rect", {
+          overwrite: "auto",
+          ease: "power2.out",
+          scaleY: 0,
+          duration: 0.4,
+        });
+      }
+    },
+    { dependencies: [hovered], scope: liRef },
+  );
+
   return (
     <li
       ref={liRef}
@@ -46,6 +116,12 @@ const ListItem = ({
           {title}
         </span>
 
+        {/* The div is for the hover */}
+        <div
+          className="absolute top-0 left-0 z-20 w-full h-full bg-red"
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        />
         {/* Highlighted text */}
         <h3
           ref={highlightedTextRef}
@@ -56,21 +132,44 @@ const ListItem = ({
         </h3>
       </div>
 
-      {/*
-       <div className="flex absolute top-0 right-0 items-center px-4 w-full h-full text-black sm:px-10 md:pl-20 lg:block lg:py-7 xl:pl-24 2xl:pl-36 bg-secondary">
-      <h3 className="hidden md:block">{maskedTitle ?? title}</h3>
-      <p
-        dangerouslySetInnerHTML={{ __html: body }}
-        className="flex right-0 items-center w-full h-full font-bold md:absolute md:top-1/2 md:pr-10 md:pl-7 md:-translate-y-1/2 xl:pr-20 text-whyme bg-secondary md:max-w-whyme xl:text-nowrap"
-      />
-    </div>
-*/}
+      {/* Masked text */}
+      <div
+        style={{
+          maskImage: `url(#mask-${title.split(" ").join("-").toLowerCase()})`,
+          maskMode: "alpha",
+        }}
+        className="flex absolute top-0 right-0 items-center px-4 w-full h-full text-black sm:px-10 md:pl-20 lg:py-7 xl:pl-24 2xl:pl-36 bg-secondary"
+      >
+        <h3 className="hidden md:block">{maskedTitle ?? title}</h3>
+        <p
+          dangerouslySetInnerHTML={{ __html: body }}
+          className="flex right-0 items-center w-full h-full font-bold md:absolute md:top-1/2 md:pr-10 md:pl-7 md:-translate-y-1/2 xl:pr-20 bg-secondary text-whyme md:max-w-whyme xl:text-nowrap"
+        />
+
+        {/* This svg to show the text that are masked */}
+        <svg
+          fill="none"
+          ref={svgRef}
+          xmlns="http://www.w3.org/2000/svg"
+          className="flex absolute top-0 left-0 flex-row w-full h-full"
+        >
+          <mask id={`mask-${title.split(" ").join("-").toLowerCase()}`}>
+            <rect
+              x="0%"
+              y="0%"
+              height="100%"
+              width="100vw"
+              className="fill-secondary"
+            />
+          </mask>
+        </svg>
+      </div>
     </li>
   );
 };
 const WhyMe = () => {
   return (
-    <section className="w-screen h-dvh">
+    <section className="overflow-hidden relative w-screen h-dvh">
       <h2 className="py-7 text-center 2xl:py-14">why me</h2>
       <ul>
         <ListItem
