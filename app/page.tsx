55de3f616a8f1e395a3e6335e51c94280b7682ce
HamzaSx <Hamzasxh@gmail.com>
2025-05-29 15:51:33 +0300

    feat(The scroll is smoother):

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
@@ -5,8 +6,18 @@ import WhyMe from "@/components/WhyMe";
 import Contact from "@/components/Contact";
 import LoadingScreen from "@/components/LoadingScreen";
 import Projects from "@/components/Projects";
+import gsap, { ScrollSmoother, ScrollTrigger } from "gsap/all";
+import { useGSAP } from "@gsap/react";
 
 export default function Page() {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  useGSAP(() => {
+    ScrollSmoother.create({
+      content: "main",
+      smooth: 2,
+      smoothTouch: 1,
+    });
+  });
   return (
     <>
       <Header />
